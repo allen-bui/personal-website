7370ec3a7af779f57cf0a696c031d4f19dc8006c
allen-bui <allen.vbui@gmail.com>
2019-11-05 16:38:54 -0800

    adds event handlers to navigation bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,15 +4,48 @@ import './header.css';
 const Header = () => {
   return (
     <table className='navigation-title-headers'>
-      <thead>
-        <td className='navigation-title'>Home</td>
-        <td className='navigation-title'>About Me</td>
-        <td className='navigation-title'>Portfolio</td>
-        <td className='navigation-title'>Contact</td>
-        <td className='navigation-title'>Resume</td>
-      </thead>
+      <tbody>
+        <tr>
+          {['Home', 'About Me', 'Portfolio', 'Contact', 'Resume'].map(
+            (header, index) => {
+              return <SingleHeader key={index} headerTitle={header} />;
+            },
+          )}
+        </tr>
+      </tbody>
     </table>
   );
 };
+
+class SingleHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHoverOver: false,
+    };
+    this.onUserHover = this.onUserHover.bind(this);
+  }
+  onUserHover() {
+    this.setState({
+      isHoverOver: !this.state.isHoverOver,
+    });
+  }
+  render() {
+    const style = {
+      cursor: this.state.isHoverOver ? 'pointer' : 'default',
+      borderBottom: this.state.isHoverOver ? '2px solid steelblue' : 'none',
+    };
+    return (
+      <td
+        className='navigation-title'
+        style={style}
+        onMouseEnter={this.onUserHover}
+        onMouseLeave={this.onUserHover}
+      >
+        {this.props.headerTitle}
+      </td>
+    );
+  }
+}
 
 export default Header;
