aa514d5001e03ac8e005f33421b8d5cd8708d286
allen-bui <allen.vbui@gmail.com>
2019-11-06 16:10:12 -0800

    fixes color scheme when user goes to different navigation

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,22 +6,42 @@ const pages = [
   {
     title: 'Home',
     alias: '/',
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#fff',
+    },
   },
   {
     title: 'About Me',
     alias: '/about',
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#000',
+    },
   },
   {
     title: 'Portfolio',
     alias: '/portfolio',
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#000',
+    },
   },
   {
     title: 'Contact',
     alias: '/contact',
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#000',
+    },
   },
   {
     title: 'Resume',
     alias: '/resume',
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#000',
+    },
   },
 ];
 
@@ -61,6 +81,7 @@ class SingleHeader extends React.Component {
   }
 
   render() {
+
     const style = {
       cursor: this.state.isHoverOver ? 'pointer' : 'default',
       borderBottom: this.state.isHoverOver ? '2px solid white' : 'none',
@@ -70,6 +91,11 @@ class SingleHeader extends React.Component {
     const linkStyle = {
       textDecoration: 'none',
       color: '#fff',
+    };
+
+    if (window.location.href.split('3000')[1] !== '/') {
+      linkStyle['color'] = '#000';
+      style['borderBottom'] = this.state.isHoverOver ? '2px solid black' : 'none';
     };
 
     return (
