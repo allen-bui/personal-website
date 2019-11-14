275dcc6c6ecd809e0f7c33e752ba6f3d37a0bcc2
allen-bui <allen.vbui@gmail.com>
2019-11-14 15:18:41 -0800

    changes placeholder text to project description text

--- src/components/portfolio/projects/blog.js
+++ src/components/portfolio/projects/blog.js
@@ -8,15 +8,8 @@ const blog = {
   title: 'PERSONAL BLOG',
   url: 'https://github.com/allen-bui/personal-blog-design',
   github: 'https://github.com/allen-bui/personal-blog-design',
-  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-  ultrices dui sapien eget mi proin. Posuere sollicitudin aliquam
-  ultrices sagittis orci a scelerisque. Pellentesque diam volutpat
-  commodo sed egestas egestas fringilla phasellus faucibus.`,
-  techStack: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-  ultrices dui sapien eget mi proin. Posuere sollicitudin aliquam
-  ultrices sagittis orci a scelerisque.`,
+  description: `Giving myself a week to build anything I wanted, this blog project came about. I built custom user authentication from scratch, where a user can register, login, and see the posts they've created. The posts are saved in a a MySQL database and retrieved when a user logs into their account. It took me a week to complete the minimum viable product (MVP, no styling) and I was happy with it, but I decided to sprinkle on some CSS to make it look and feel like an actual blog.`,
+  techStack: `React, Node.js, Express, MySQL`,
 };
 
 export default blog;
