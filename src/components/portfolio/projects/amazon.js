dd7e0056ec3e8cb6adad7cc13fb3d9903ef3cd88
allen-bui <allen.vbui@gmail.com>
2019-11-14 15:36:32 -0800

    changes placeholder text to actual project description and tech stack used

--- src/components/portfolio/projects/amazon.js
+++ src/components/portfolio/projects/amazon.js
@@ -1,14 +1,14 @@
 const amazon = {
   images: [
-    { src: 'https://i.imgur.com/x2Kv055.png' },
+    { src: 'https://imgur.com/MBK7hQU.png' },
     { src: 'https://i.imgur.com/AX8YzoG.png' },
     { src: 'https://i.imgur.com/jR4TO0S.png' },
   ],
   title: 'AMAZON SYSTEM DESIGN',
   url: 'https://docs.google.com/presentation/d/10qseyfegiWEMIQX8gebnRM7PbF4t7S0PDjhgjaMNVfE/edit?usp=sharing',
   github: 'https://github.com/allen-bui/amazon-system-design',
-  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  techStack: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat lacus laoreet non curabitur gravida. Urna porttitor rhoncus dolor purus non enim praesent. Pharetra sit amet aliquam id diam maecenas ultricies mi.`,
+  description: `This project was focused on improving the backend performance of an existing microservice. I chose to improve another engineer's Amazon product microservice. I tinkered with the optimization a lot by swapping out the existing Mongo database with PostgreSQL and Cassandra to see if it would make a difference. After choosing a database, I put the microservice on an EC2 instance and played with vertical scaling to see if I could optimize query speeds. I was able to push my system to 1000 requests per second (RPS) with a 0% error rate on the free tier of my EC2 instance. I'm pretty happy with how everything turned out because this was my first backend focused project.`,
+  techStack: `Node.js, Express, Cassandra, PostgreSQL, AWS EC2`,
 };
 
 export default amazon;
