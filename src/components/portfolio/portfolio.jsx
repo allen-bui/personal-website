d6acd2b3b6fa224faafb3f0c89b7ead060910ee8
allen-bui <allen.vbui@gmail.com>
2020-02-24 00:12:57 -0800

    decouples duplicate code in portfolio file and adds trivia now project

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -7,71 +7,72 @@ import './portfolio.css';
 import airbnb from './projects/airbnb.js';
 import amazon from './projects/amazon.js';
 import blog from './projects/blog.js';
-
+import trivia from './projects/trivia.js';
 
 class Portfolio extends React.Component {
-
   constructor() {
     super();
     this.state = {
       projectName: 'Airbnb Clone',
-    }
+    };
     this.getCurrentValue = this.getCurrentValue.bind(this);
   }
 
   getCurrentValue(event) {
     this.setState({
       projectName: event.target.value,
-    })
+    });
   }
 
   render() {
+    const wrapper = (
+      <div className="project-list-container">
+        <div className="project-list-title">
+          PROJECTS:
+          <select className="project-name" onChange={this.getCurrentValue}>
+            <option>Airbnb Clone</option>
+            <option>Trivia Now</option>
+            <option>Personal Blog Project</option>
+            <option>Amazon System Design</option>
+          </select>
+        </div>
+      </div>
+    );
 
     if (this.state.projectName === 'Airbnb Clone') {
       return (
         <div>
           <Header />
-          <div className='project-list-container'>
-            <div className='project-list-title'>
-              PROJECTS:
-              <select className='project-name' onChange={this.getCurrentValue}>
-                <option>Airbnb Clone</option>
-                <option>Personal Blog Project</option>
-                <option>Amazon System Design</option>
-              </select>
-            </div>
-          </div>
+          {wrapper}
           <Project name={airbnb} />
         </div>
       );
-    }
-    else if (this.state.projectName === 'Amazon System Design') {
+    } else if (this.state.projectName === 'Trivia Now') {
       return (
         <div>
           <Header />
-          <div className='project-list-container'>
-            <div className='project-list-title'>
-              PROJECTS:
-              <select className='project-name' onChange={this.getCurrentValue}>
-                <option>Airbnb Clone</option>
-                <option>Personal Blog Project</option>
-                <option>Amazon System Design</option>
-              </select>
-            </div>
-          </div>
+          {wrapper}
+          <Project name={trivia} />
+        </div>
+      );
+    } else if (this.state.projectName === 'Amazon System Design') {
+      return (
+        <div>
+          <Header />
+          {wrapper}
           <Project name={amazon} />
         </div>
       );
-    }
-    else if (this.state.projectName === 'Personal Blog Project') {
+    } else if (this.state.projectName === 'Personal Blog Project') {
       return (
         <div>
           <Header />
-          <div className='project-list-container'>
-            <div className='project-list-title'>
+          <div className="project-list-container">
+            <div className="project-list-title">
               PROJECTS:
-              <select className='project-name' onChange={this.getCurrentValue}>
+              <select className="project-name" onChange={this.getCurrentValue}>
                 <option>Airbnb Clone</option>
+                <option>Trivia Now</option>
                 <option>Personal Blog Project</option>
                 <option>Amazon System Design</option>
               </select>
@@ -82,6 +83,6 @@ class Portfolio extends React.Component {
       );
     }
   }
-};
+}
 
 export default Portfolio;
---
+++ src/components/portfolio/projects/trivia.js
@@ -0,0 +1,14 @@
+const trivia = {
+  images: [
+    { src: 'https://i.imgur.com/LwkXVjK.png' },
+    { src: 'https://imgur.com/yci0sFt.png' },
+    { src: 'https://imgur.com/uZj9feE.gif' },
+  ],
+  title: 'TRIVIA NOW APPLICATION',
+  url: 'https://trivia-now.netlify.com/',
+  github: 'https://github.com/allen-bui/trivia-app',
+  description: `This project stemmed from wanting to educate the world a little bit more in a fun way. I couldn't find any cool trivia apps online, so I built my own! I recently learned TypeScript, React Hooks, and SASS – so I built this application using those technologies because I wanted to reinforce my learning. I'm really happy with the way it turned out and plan to add the following features in the future – a timer per question, categories, and more animations. Stay tuned!`,
+  techStack: `TypeScript, React, React Hooks, CSS/SASS`,
+};
+
+export default trivia;
